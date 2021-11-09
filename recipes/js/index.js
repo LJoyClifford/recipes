35a6f9c6179a8d13061bce343cfab0f7458c0fa4
Lisa Clifford <ljoyclifford@gmail.com>
2021-11-09 15:59:00 -0500

    Update css color scheme and fonts

---
+++ recipes/js/index.js
@@ -0,0 +1,11 @@
+// create variables for the sections that need to be targeted
+
+// const handlePageLoaded = () => {
+// attach the submit listener to the search form
+//}
+// document.addEventListener("DOMContentLoaded", handlePageLoaded )
+
+
+
+
+ 
